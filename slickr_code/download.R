ad47bda7f63d6b93d97834c352d2d911a5c486c7
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2024-01-03 10:18:27 -0500

    added more pages, a slickr and updated _quarto.yml

---
+++ slickr_code/download.R
@@ -0,0 +1,8 @@
+
+# make slickR slides for d
+library(slickR)
+
+download <- c("images/downloading/uncheck_evt_1.png", "images/downloading/start_tool_2.png")
+download_slick <- slickR(obj = download, height=600, width='100%') 
+download_slick
+
